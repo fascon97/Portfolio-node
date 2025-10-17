4d406cdd07ae861122a8c9cf649617a37dbc0e23
fascon97 <fassu_conde97@hotmail.com>
2025-10-17 05:08:47 +0200

    Refactor typed.js code

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs")
+const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
   res.render('home');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs")
+const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
   res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = router;
